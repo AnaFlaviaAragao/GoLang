2c87d70fad99a3c8e4953ac832bceafc4f894bbe
Ana Flavia Aragao <anaflavia@eng.ci.ufpb.br>
2020-08-19 23:01:01 -0300

    Update

---
+++ Controles/switch3/switch3.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func tipo(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "inteiro"
+	case float64:
+		return "real"
+	case string:
+		return "string"
+	case func():
+		return "função"
+	default:
+		return "não sei"
+	}
+
+}
+
+func main() {
+
+	fmt.Println(tipo(2.4))
+	fmt.Println(tipo(1))
+	fmt.Println(tipo("OPA"))
+	fmt.Println(tipo(func() {}))
+
+}
