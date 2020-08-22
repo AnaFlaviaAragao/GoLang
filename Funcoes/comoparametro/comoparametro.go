e8c4c953e25681065144eca1a911cb464a176fd6
Ana Flavia Aragao <anaflavia@eng.ci.ufpb.br>
2020-08-22 15:28:50 -0300

    Update

--- Funcoes/comoparametro/comoparametro.go
+++ Funcoes/comoparametro/comoparametro.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func multiplicação(a, b int) int {
+	return a * b
+}
+
+//map, reduce, filter
+
+func exec(funcao func(int, int) int, p1, p2 int) int {
+	return funcao(p1, p2)
+}
+
+func main() {
+	resultado := exec(multiplicação, 3, 4)
+	fmt.Println(resultado)
 }
